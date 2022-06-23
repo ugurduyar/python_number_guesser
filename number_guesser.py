03dcd1ae5c73772112a8b235a255b638e44300e2
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 17:12:18 +0300

    fixed answer count and removed fixed game values

--- number_guesser.py
+++ number_guesser.py
@@ -1,11 +1,13 @@
 import random
-
 # Make a random number
-number = random.randrange(0,500)
-# This will generate a number between 0 and 500
-answer_count = 1
+minimum_number = 0
+maximum_number = 500
+number = random.randrange(minimum_number,maximum_number)
+# ^ This will generate a number between 0 and 500
+answer_count = 0
 # Next line is explaining the game and asking for a number to initialize
-answer = int(input("I have generated a number between 0 and 500, lets try to guess it! Type a number here and i will tell you if its higher or lower, lets start! "))
+answer = int(input("I have generated a number between {} and {}, lets try to guess it! Type a number here and i will tell you if its higher or lower, lets start! ".format(minimum_number,maximum_number)))
+answer_count += 1
 # While loop to check the value
 while answer != number:
     answer_count += 1
