5fcc49ce447bb22bb7884fefa30a88130384ac4c
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 17:05:32 +0300

    added while loop and answer counter

--- number_guesser.py
+++ number_guesser.py
@@ -2,5 +2,17 @@ import random
 
 # Make a random number
 number = random.randrange(0,500)
+print(number)
 # This will generate a number between 0 and 500
+answer_count = 1
+answer = int(input("I have generated a number between 0 and 500, lets try to guess it! Type a number here and i will tell you if its higher or lower, lets start! "))
 
+while answer != number:
+    answer_count += 1
+    if answer > number:
+        answer = int(input(("Lower ")))
+        continue
+    else:
+        answer = int(input(("Higher ")))
+if answer == number:
+    print("Congratulations! You've guessed the number in {} tries!".format(answer_count))
